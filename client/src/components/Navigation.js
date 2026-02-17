fb935c4532a418181dcd5f14029b4e3d8ec636e7
HannaBProjects <hannabokariuk@gmail.com>
2026-02-17 23:46:13 +0000

    move Login/Register/Reset actions into top navigation

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -4,9 +4,16 @@ import { Link } from "react-router-dom";
 export const Navigation = () => {
     return (
         <nav className="top-nav">
-            <Link to="/" className="top-nav-link">Home</Link>
-            <Link to="/AddProduct" className="top-nav-link">Add</Link>
-            <Link to="/DisplayAllProducts" className="top-nav-link">All Products</Link>
+            <div className="top-nav-left">
+                <Link to="/" className="top-nav-link">Home</Link>
+                <Link to="/AddProduct" className="top-nav-link">Add</Link>
+                <Link to="/DisplayAllProducts" className="top-nav-link">All Products</Link>
+            </div>
+            <div className="top-nav-right">
+                <Link to="/Login" className="top-nav-link">Login</Link>
+                <Link to="/Register" className="top-nav-link">Register</Link>
+                <Link to="/ResetDatabase" className="top-nav-link top-nav-link-danger">Reset Database</Link>
+            </div>
         </nav>
     );
 };
