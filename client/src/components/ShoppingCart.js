9dd20ab9ce5fa620b727c39607604536d9bdec95
nikitsya <nikitasmiichyk@gmail.com>
2026-02-25 23:41:07 +0000

    Scaffold ShoppingCart component

---
+++ client/src/components/ShoppingCart.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const ShoppingCart = () => {
+    return (
+        <div className="form-container">
+            <h2>Shopping Cart</h2>
+        </div>
+    )
+}
