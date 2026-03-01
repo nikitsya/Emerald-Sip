8620bc8540e5f9caee35aacb91f2376028d9b057
nikitsya <nikitasmiichyk@gmail.com>
2026-03-01 06:10:57 +0000

    docs(client): document shared ProductFormFields component

--- client/src/components/ProductFormFields.js
+++ client/src/components/ProductFormFields.js
@@ -2,6 +2,7 @@ import React from "react"
 import {Link} from "react-router-dom"
 import {Button} from "./Button"
 
+// Shared product form fields used by both Add and Edit pages.
 export const ProductFormFields = ({formValues, onFieldChange, submitLabel, onSubmit}) => (
     <form>
         <label>Name</label>
